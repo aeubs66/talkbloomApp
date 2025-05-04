d586f00c2e41d45be305126ffaa683805f177ad0
aeubs11 <aeubs65@gmail.com>
2025-05-04 22:25:42 +0300

    admin update

--- lib/admin.ts
+++ lib/admin.ts
@@ -1,7 +1,8 @@
 import { auth } from "@clerk/nextjs";
 
 const allowedId = [
-  "user_2sehoQlGYc9wgPARxdzDuY10K6p"
+  "user_2sehoQlGYc9wgPARxdzDuY10K6p",
+  "user_2vX9rDvaGp9NtXWm7hIQvTjkFTf"
 ]
 
 export const getIsAdmin = () => {
